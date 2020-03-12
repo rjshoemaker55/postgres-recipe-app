be45d307035053d1b722a00f1438714ba8acdab6
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-03-11 21:15:51 -0400

    app connected to postgres database

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client } = require('pg');
+const client = new Client({
+  user: 'postgres',
+  password: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  database: 'recipebook'
+});
+
+client
+  .connect()
+  .then(() => console.log('Connected to Postgres database.'))
+  .then(() => client.query('SELECT * from recipes'))
+  .then(res => console.log(console.table(res.rows)))
+  .catch(e => console.log(e))
+  .finally(() => client.end());
